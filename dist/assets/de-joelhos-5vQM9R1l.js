fa89cfa75581ab64486767ef65512043e76f2aed
Aldo Monteiro <alduxx@gmail.com>
2025-11-14 12:07:33 -0300

    Ajuste na poesia

--- dist/assets/de-joelhos-5vQM9R1l.js
+++ dist/assets/de-joelhos-5vQM9R1l.js
@@ -1,23 +1,24 @@
----
+const e=`---
 title: De Joelhos
 date: 2025-11-14
 excerpt: Florbela Espanca
 ---
 
 Bendita seja a Mãe que te gerou.
-Bendito o leite que te fez crescer.  
-Bendito o berço aonde te embalou  
+Bendito o leite que te fez crescer.
+Bendito o berço aonde te embalou
 A tua ama, pra te adormecer!
 
-Bendita essa canção que acalentou  
-Da tua vida o doce alvorecer…  
-Bendita seja a lua que inundou  
+Bendita essa canção que acalentou
+Da tua vida o doce alvorecer…
+Bendita seja a lua que inundou
 De luz, a terra, só para te ver…
 
-Benditos sejam todos que te amarem,  
-As que em volta de ti ajoelharem,  
+Benditos sejam todos que te amarem,
+As que em volta de ti ajoelharem,
 Numa grande paixão fervente e louca!
 
 E se mais que eu, um dia, te quiser
 Alguém, bendita seja essa Mulher,
 Bendito seja o beijo dessa boca!!
+`;export{e as default};
