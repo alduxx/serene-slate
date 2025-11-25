488a320877597ab3f43d07c5ea8fb5b82dcb4b2d
Aldo Monteiro <alduxx@gmail.com>
2025-11-25 14:36:22 -0300

    Update Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -26,7 +26,9 @@ const Hero = () => {
           </h1>
           
           {/* Bio */}
-          <p className="text-xl md:text-2xl text-muted-foreground mb-12 max-w-2xl mx-auto font-light">Este site é um repositório pessoal de temas que despertam meu interesse e que me ajudam a praticar a escrita, além de reunir poesias selecionadas que devem ser compartilhar com o mundo.</p>
+          <p className="text-xl md:text-2xl text-muted-foreground mb-12 max-w-2xl mx-auto font-light">
+            Escrevo aqui sobre temas que despertam meu interesse e onde coleciono poesias selecionadas.
+          </p>
           
           {/* CTA Buttons */}
           <div className="flex flex-wrap gap-4 justify-center">
